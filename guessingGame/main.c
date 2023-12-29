541da5f3dcba0e4f34263c542fc26425079d5255
sustagantos <gustavo.s.goncalves@unesp.br>
2023-12-29 19:45:47 -0300

    guessingGame added

--- guessingGame/main.c
+++ guessingGame/main.c
@@ -9,9 +9,9 @@ void main() {
     printf("GUESSING GAME!");
 
     int guess;
-    int acertou;
+    int gotIt;
     int dificulty;
-    int totaldetentativas;
+    int maxTries;
     double score = 1000;
 
     //generating the secret number
@@ -19,84 +19,62 @@ void main() {
     int number = rand() % 100;
 
     //choosing dificulty
-
-    printf("Qual o nível de dificuldade?\n");
-    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
-    printf("Escolha: ");
+    printf("choosing the dificulty!\n");
+    printf("(1)Easy (2)Medium (3)Hard\n\n");
+    printf("choose: ");
 
     scanf("%d", &dificulty);
 
     switch(dificulty) {
         case 1: 
-            totaldetentativas = 20;
+            maxTries = 20;
             break;
         case 2:
-            totaldetentativas = 15;
+            maxTries = 15;
             break;
         default:
-            totaldetentativas = 6;
+            maxTries = 6;
             break;
     }
 
-    // loop principal do jogo
-    for(int i = 1; i <= totaldetentativas; i++) {
+    //game loop
+    for(int i = 1; i <= maxTries; i++) {
 
-        printf("-> Tentativa %d de %d\n", i, totaldetentativas);
+        printf("%d out of %d tries\n", i, maxTries);
 
-        printf("guess um número: ");
+        printf("guess a number: ");
         scanf("%d", &guess);
 
-        // tratando guess de numero negativo
+        //negative numbers
         if(guess < 0) {
-            printf("Você não pode chutar números negativos\n");
+            printf("choose a positive number!\n");
             i--;
             continue;
         }
 
-        // verifica se acertou, foi maior ou menor
-        acertou = guess == number;
+        //checks if guess is right
+        gotIt = guess == number;
 
-        if(acertou) {
+        if(gotIt) {
             break;
         } else if(guess > number) {
-            printf("\nSeu guess foi maior do que o número secreto!\n\n");
+            printf("\nGuess was bigger than secret number!\n\n");
         } else {
-            printf("\nSeu guess foi menor do que o número secreto!\n\n");
+            printf("\nGuess was smaller than secret number\n\n");
         }
 
-        // calcula a quantidade de score
-        double scoreperdidos = abs(guess - number) / 2.0;
-        score = score - scoreperdidos;
+
+        double scoreLost = abs(guess - number) / 2.0;
+        score = score - scoreLost;
     }
 
-    // imprimindo mensagem de vitoria ou derrota
+    //printing last message
     printf("\n");
-    if(acertou) {
-        printf("             OOOOOOOOOOO               \n");
-        printf("         OOOOOOOOOOOOOOOOOOO           \n");
-        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
-        printf("    OOOOOO      OOOOO      OOOOOO      \n");
-        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
-        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
-        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
-        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
-        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
-        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
-        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
-        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
-        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
-        printf("         OOOOOO         OOOOOO         \n");
-        printf("             OOOOOOOOOOOO              \n");
-        printf("\nParabéns! Você acertou!\n");
-        printf("Você fez %.2f score. Até a próxima!\n\n", score);
+    if(gotIt) {
+        printf("\nCongratulations! You guessed the secret number!\n");
+        printf("SCORE: %.2f\n\n", score);
     } else {
-
-        printf("       \\|/ ____ \\|/    \n");   
-        printf("        @~/ ,. \\~@      \n");   
-        printf("       /_( \\__/ )_\\    \n");   
-        printf("          \\__U_/        \n");
-
-        printf("\nVocê perdeu! Tente novamente!\n\n");
+        printf("\nYou couldn't guess the number! Try again!\n\n");
     }
 
 
