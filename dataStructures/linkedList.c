5ac8f3b6a239c27fe01af49f80afa9ccfe3533e4
sustagantos <gustavo.s.goncalves@unesp.br>
2024-01-06 19:13:52 -0300

    minor changes

--- dataStructures/linkedList.c
+++ dataStructures/linkedList.c
@@ -5,7 +5,7 @@
 #include "linkedList.h"
 
 
-linkedList* listInit(){        //allocs memory for Linked List, and first->NULL
+linkedList* listInit(){  //allocs memory for Linked List, and first->NULL
     
     linkedList *l = (linkedList *) malloc (sizeof(linkedList));
     l->first = NULL;
@@ -13,61 +13,79 @@ linkedList* listInit(){        //allocs memory for Linked List, and first->NULL
 
 }
 
-node* createNode(int value){        //allocs memory for Linked List Node, and puts a value in it
+node* createNode(int value){  //allocs memory for Linked List Node, and puts a value in it
 
-    node *n = (node *) malloc (sizeof(node));   //creates node and allocs memory to it
+    node *n = (node *) malloc (sizeof(node));  //creates node and allocs memory to it
     n->info = value;
     n->next = NULL;
     return n;
     
 }
 
-void addToList(linkedList *l, int value) {     //adds a number to the list, not in order
+void addToList(linkedList *l, int value) {  //adds a number to the list, not in order
 
-    node *newNode = createNode(value);      //creates a new node, with parameter value
-    node *aux = l->first;       //aux = the first list item
+    node *newNode = createNode(value);  //creates a new node, with parameter value
+    node *aux = l->first;  //aux = the first list item
 
-    if (aux == NULL) {      //if aux is NULL, means list is empty, then adds to 1st position
+    if (aux == NULL) {  //if aux is NULL, means list is empty, then adds to 1st position
         l->first = newNode;
 
     }else{
     
-        while (aux->next != NULL){    //goes to pre-last
+        while (aux->next != NULL){  //goes to pre-last
             aux = aux->next;
         }
 
-        newNode->next = NULL;      //new node is last element, therefore, points to NULL
+        newNode->next = NULL;  //new node is last element, therefore, points to NULL
         aux->next = newNode;
     }
 
 }
     
-void printList(linkedList *l){      //prints list
+void printList(linkedList *l){  //prints list
 
     node *aux = l->first;       
-    printf("\nlinked list: ");
+    printf("linked list: ");
 
-    while(aux!=NULL){       //while node isn't NULL
-        printf("%d, ",aux->info);    //prints its info
-        aux=aux->next;      //goes to next
+    while(aux!=NULL){  //while node isn't NULL
+        printf("%d, ",aux->info);  //prints its info
+        aux=aux->next;  //goes to next
     }
-    printf("\n\n");
+    printf("\n");
 
 }
     
+void findInList(linkedList *l, int value){  //finds in list, if there is to find
+    
+    node *aux = l->first;
 
+    while(aux != NULL){
+        if(aux->info == value){
+            printf("%d is in the list!\n",value);
+            return;
+        }
+        aux = aux->next;
+    }
+
+    printf("%d is not in the list!\n",value);
+
+}
+
+void removeFromList(linkedList *l, int value){
+    
+}
 
 void main(){
 
-    linkedList *list = listInit();      //declare and allocate a linked list
+    linkedList *list = listInit();  //declare and allocate a linked list
 
-    addToList(list, 5);
-    addToList(list, 4);
-    addToList(list, 3);
-    addToList(list, 2);
-    addToList(list, 1);       
+    for(int i = 0; i <= 20; i++){   //adds 1-20 in the list
+        addToList(list, i);
+    }
 
+    printList(list);
 
-    printList(list);        //print the list
+    findInList(list, 18);
+    findInList(list, 23);
 
 }
